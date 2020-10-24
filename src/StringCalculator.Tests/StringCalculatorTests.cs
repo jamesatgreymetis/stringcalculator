cfb4bf49f4a0a68624329da4f50e861531f1869c
James Bradshaw <james@greymetis.co.uk>
2020-10-24 20:20:32 +0100

    Complete String Calculator implementation

---
+++ src/StringCalculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace StringCalculator.Tests
+{
+    public class StringCalculatorTests
+    {
+        private readonly StringCalculator _sut;
+
+        public StringCalculatorTests()
+        {
+            _sut = new StringCalculator();
+        }
+
+        [Theory]
+        [InlineData("", 0)]
+        [InlineData("1", 1)]
+        [InlineData("2", 2)]
+        void Single_number_input_should_return_number(string input, int expectedOutput)
+        {
+            // Act
+            var result = _sut.Add(input);
+
+            // Assert
+            result.Should().Be(expectedOutput);
+        }
+
+        [Fact]
+        void Comma_delimited_number_sequence_input_should_return_the_sum()
+        {
+            var input = "1,2,3";
+            var expectedOutput = 6;
+            
+            // Act
+            var result = _sut.Add(input);
+
+            // Assert
+            result.Should().Be(expectedOutput);
+        }
+
+        [Fact]
+        void number_sequence_has_newline_character_should_return_the_sum()
+        {
+            // Arrange
+            var input = "1\n2,3";
+            var expectedOutput = 6;
+
+            // Act
+            var result = _sut.Add(input);
+
+            // Assert
+            result.Should().Be(expectedOutput);
+        }
+
+        [Fact]
+        void invalid_number_sequence_should_throw_exception()
+        {
+            // Arrange
+            var input = "1,\n";
+
+            // Act
+            Action act = () => _sut.Add(input);
+
+            // Assert
+            act.Should().Throw<ArgumentException>().WithMessage("Invalid character in sequence.");
+        }
+
+        [Theory]
+        [InlineData("//;\n1;2", 3)] // single delimiter
+        [InlineData("//*%\n1*2%3", 6)] // multiple delimiter
+        void Custom_delimiter_with_valid_number_sequence_should_return_the_sum(string input,int expectedResult)
+        {
+            // Act
+            var result = _sut.Add(input);
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+
+        [Theory]
+        [InlineData("1,2,3,", "-4")]
+        [InlineData("1,2,3,", "-4,-5,-6")]
+        void negative_number_in_number_sequence_should_throw_exception(string validSequence, string negativeNumbers)
+        {
+            // Arrange
+            var input = $"{validSequence}{negativeNumbers}";
+
+            // Act
+            Action act = () => _sut.Add(input);
+
+            // Assert
+            act.Should().Throw<ArgumentException>().WithMessage($"Negatives not allowed: {negativeNumbers}");
+        }
+
+        [Theory]
+        [InlineData("1001,2,13", 15)]
+        [InlineData("2,1001,13", 15)]
+        [InlineData("2,13,1001", 15)]
+        void number_greater_than_1000_in_number_sequence_should_be_excluded_from_sum(string input, int expectedResult)
+        {
+            // Act
+            var result = _sut.Add(input);
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+    }
+}
---
+++ src/StringCalculator/StringCalculator.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Linq;
+
+namespace StringCalculator
+{
+    public class StringCalculator
+    {
+        /// <summary>
+        /// The default delimiter used if a custom delimiter is not specified.
+        /// </summary>
+        public const char DefaultDelimiter = ',';
+
+        private const string CustomerDelimiterIndicator = "//";
+
+        /// <summary>
+        /// Adds a delimited sequence of numbers.
+        /// </summary>
+        /// <param name="input">the input to sum.</param>
+        /// <returns>sum of the input.</returns>
+        /// <exception cref="ArgumentException">Thrown if the input contains negative numbers or an invalid sequence.</exception>
+        public int Add(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return 0;
+            }
+
+            var delimiter = ResolveDelimiter(input);
+
+            var cleanedInput = CleanInput(input, delimiter);
+
+            ValidateInputSequence(cleanedInput);
+
+            var intArray = ConvertToIntArray(cleanedInput, delimiter);
+
+            ValidateArrayForNegativeNumbers(intArray);
+
+            return Sum(intArray);
+        }
+
+        private static int Sum(int[] intArray)
+        {
+            return intArray.Where(i=> i <= 1000).Sum();
+        }
+
+        /// <summary>
+        /// Converts the string input to an int array
+        /// </summary>
+        /// <param name="cleanedInput">the cleans input to convert.</param>
+        /// <param name="delimiter">the delimiters in the input.</param>
+        /// <returns></returns>
+        private static int[] ConvertToIntArray(string cleanedInput, char[] delimiter)
+        {
+            return cleanedInput
+                .Split(delimiter)
+                .Select(int.Parse)
+                .ToArray();
+        }
+
+        /// <summary>
+        /// Validates sequence for negative numbers.
+        /// </summary>
+        /// <param name="array">The array to validate.</param>
+        /// <exception cref="ArgumentException">Thrown if the array contains negative numbers.</exception>
+        private void ValidateArrayForNegativeNumbers(int[] array)
+        {
+            if (array.Any(i => i < 0))
+            {
+                throw new ArgumentException($"Negatives not allowed: {string.Join(',', array.Where(i=>i < 0).Select(i=>i))}");
+            }
+        }
+
+        /// <summary>
+        /// Resolves if the input has a custom delimiter specified.
+        /// </summary>
+        /// <param name="input">the input to analyse</param>
+        /// <returns>Returns rue if a custom delimiter has been specified.</returns>
+        private bool ContainsCustomDelimiter(string input)
+        {
+            return input.StartsWith(CustomerDelimiterIndicator);
+        }
+
+        /// <summary>
+        /// Gets the customer delimiter(s) or returns the default delimiter.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns>An array of delimiters</returns>
+        private char[] ResolveDelimiter(string input)
+        {
+            if (ContainsCustomDelimiter(input))
+            {
+                var delimiter1 = char.Parse(input.Substring(2, 1));
+
+                if (input.IndexOf('\n') == 4)
+                {
+                    var delimiter2 = char.Parse(input.Substring(3, 1));
+                    return new[] { delimiter1, delimiter2 };
+                }
+
+                return new[] {delimiter1};
+            }
+
+            return new[] {DefaultDelimiter};
+        }
+
+        /// <summary>
+        /// Remove the custom delimiter from the input and converts newline characters to a valid delimiter.
+        /// </summary>
+        /// <param name="input">The input to be cleaned.</param>
+        /// <param name="delimiter">The delimiter(s) in the sequence.</param>
+        /// <returns>A cleaned sequence of delimited numbers</returns>
+        private string CleanInput(string input, char[] delimiter)
+        {
+            if (ContainsCustomDelimiter(input))
+            {
+                input = input.Replace($"{CustomerDelimiterIndicator}{string.Concat(delimiter)}\n", "");
+            }
+
+            return input.Replace('\n', DefaultDelimiter);
+        }
+
+        /// <summary>
+        /// Validated the input has a valid sequence.
+        /// </summary>
+        /// <param name="input">the input to validate.</param>
+        /// <exception cref="ArgumentException">Thrown is the sequence is not valid.</exception>
+        private void ValidateInputSequence(string input)
+        {
+            if (input.EndsWith(DefaultDelimiter))
+            {
+                throw new ArgumentException("Invalid character in sequence.");
+            }
+        }
+    }
+}
